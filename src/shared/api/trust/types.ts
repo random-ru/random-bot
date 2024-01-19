c90f0ca4bd188e1f02f9f792eea4e5cfa5866c6c
Elnar Ibragimov <djelnar@gmail.com>
2024-01-19 02:34:12 +0200

    Add TrustAnalytics type

--- src/shared/api/trust/types.ts
+++ src/shared/api/trust/types.ts
@@ -12,4 +12,58 @@ export interface TrustAnalytics {
   accuracy: number
   verdict: TrustVerdict
   report_creation_date: number
+  trust_factor: number
+  user_id: number
+  correction_version: number
+  issuer: Issuer
+  factors: Factor[]
+}
+
+export interface Factor {
+  sampler: string
+  score: number
+  accuracy: number
+  max_score: number
+  expando?: Expando
+}
+
+export interface Expando {
+  gradient: Point
+  polynomial: Point
+}
+
+export interface Point {
+  s: number
+  t: string
+}
+
+export interface Issuer {
+  issuer_user_id: number
+  id: string
+  report: Report
+  report_id: string
+  kind: string
+}
+
+export interface Report {
+  id: string
+  discover: Discover
+  trust_report: TrustReport
+  user_id: number
+  issuer_id: string
+}
+
+export interface Discover {
+  message_id: number
+  user_id: number
+}
+
+export interface TrustReport {
+  user_id: number
+  report_record_id: string
+  id: string
+  factors: Factor[]
+  accuracy: number
+  report_creation_date: number
+  verdict: string
 }
